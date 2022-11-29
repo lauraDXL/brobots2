9afe51a1be191c24bb22e484a69568ab9f59a952
lauraDXL <77843343+lauraDXL@users.noreply.github.com>
2022-11-29 21:23:19 +0200

    Add files via upload

---
+++ team 2022.js
@@ -0,0 +1,103 @@
+
+import calin from '../assets/team/Calin.png';
+import luca from '../assets/team/Luca.png';
+import lara from '../assets/team/Lara.png';
+import razvan from '../assets/team/Razvan.png';
+import edii from '../assets/team/edii.jpeg';
+import sebiii from '../assets/team/sebiii.jpeg';
+import alexiaa from '../assets/team/alexiaa.jpeg';
+export const team = [
+
+  
+
+  
+
+  {
+    name: 'Răzvan Mutuleasa',
+    img: razvan,
+    phone: '0747910051',
+    instagram: 'https://www.instagram.com/razvan.mihai.24/',
+    description_en:
+      "I am cheerful and I like to meet as many people as possible. I am passionate about video science, drawing, philosophy and computer games, but from time to time I like to practice playing the guitar. I also like to give speeches and presentations.",
+    description:
+      'Sunt o fire veselă și îmi place să cunosc cât mai mulți oameni. Sunt pasionat de informatică, desenat, filosofie și jocuri pe calculator dar din când în când îmi place să exersez cântatul la chitară. De asemenea, îmi place să țin discursuri și prezentări',
+      
+  },
+  {
+    name: 'Vladutu Luca',
+    img: luca,
+    description_en:
+      "I am cheerful and I like to meet as many people as possible. I am passionate about video science, drawing, philosophy and computer games, but from time to time I like to practice playing the guitar. I also like to give speeches and presentations.",
+    description:
+      'Sunt o fire veselă și îmi place să cunosc cât mai mulți oameni. Sunt pasionat de informatică, desenat, filosofie și jocuri pe calculator dar din când în când îmi place să exersez cântatul la chitară. De asemenea, îmi place să țin discursuri și prezentări',
+      
+  },
+ 
+ 
+  {
+    name: 'Ariciu Călin',
+    instagram: "https://instagram.com/ariciucalin?utm_medium=copy_link",
+    img: calin ,
+    description_en:
+      "My passions are cars, music, gaming and everything related to technology. I am interested in robotics because I consider it a field of the future, which is constantly evolving.",
+    description:
+      'Pasiunile mele sunt mașinile, muzica, gaming-ul și tot ce ține de tehnologie. Sunt interesat de robotică deoarece consider că este un domeniu de viitor, care este în continuă evoluție.',
+    
+  },
+  {
+    name: 'Sminticiuc Alexia',
+    img:alexiaa,
+    description_en:
+      "Hi! I'm Alexia. I'm passionate about crypto, IT and reading. I define myself as ambition and perseverance. My passions are programming and design. I collaborate well with human types, I like working in a team and learning new things.",
+    description:
+      'Buna!Sunt Alexia.Sunt pasionata crypto, domeniul it si lectura.Ma definesc ambitia si perseverenta.Ca pasiuni am programarea si proiectarea.Colaborez bine cu tipologiiile umane, imi place munca in echipa si sa invat lucruri noi.',
+    
+  },
+  {
+    name: 'Mihăilescu Lara',
+    img: lara ,
+    instagram: "https://www.instagram.com/steff_tifi/",
+    description_en:
+      'I am very ambitious and perfectionist, sometimes even stubborn. My favorite song is "Aristocats". In my free time I love to dance and play the piano.',
+    description:
+      'Sunt o fire foarte ambițioasă si perfecționistă, câteodată chiar încăpățânată. Melodia mea preferată este "Aristocats". În timpul liber ador să dansez și să cânt la pian.',
+    
+  },
+  
+  {
+    name:'Dumitrescu Antonio',
+    description_en:
+    "i like programming, i don't like sports, i like reading english literature",
+    description:
+    "imi place programarea, nu imi plac sporturile, imi place sa citesc literatura in engleza",
+  },
+  {
+    name:'Tomulescu Eduard',
+    img:edii,
+    instagram: "https://www.instagram.com/edw_ard__/",
+    description_en:
+    "I love learning new things. That's why I'm passionate about IT, assembly, video games and Egyptian mythology. I consider myself a bold, friendly and calm person. Programming has helped me a lot to develop my logic and thinking and made me realize that any problem has a solution.",
+    description:
+    "Iubesc sa invat lucruri noi.De aceea sunt pasionat de IT , asamblare , jocuri video si de mitologia Egipteană .Ma consider o persoana îndrăzneață,prietenoasa si calma.Programarea m-a ajutat foarte mult sa-mi dezvolt logica si gandirea si m-a facut sa realizez ca orice problema are o solutie .",
+  },
+  {
+    name:'Galbenus Sebastian',
+    img: sebiii,
+    instagram: "https://www.instagram.com/sebi.brian/",
+    phone: '0743917533',
+    description_en:
+    "Hello, my name is Sebi! I am an American citizen who lives and studies in Romania. I am extroverted and very passionate about cooking, bodybuilding, photography and science, especially physics. My dream is to become a researcher in quantum physics and complete string theory.",
+    description:
+    "Salut, mă numesc Sebi! Sunt un cetățean American care locuiește și studiază în România. Sunt extrovertit și foarte pasionat de gătit, bodybuilding, fotografie și știință, în special fizică. Visul meu este să devin cercetător în fizica cuantică și să completez Teoria Corzilor.",
+
+  },
+
+   {
+    name:'Vladutu Luca',
+   
+
+  },
+
+ 
+
+];
