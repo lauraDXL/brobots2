6c24eafdc95ed06b389f86586d4a5f5da4fc584b
lauraDXL <77843343+lauraDXL@users.noreply.github.com>
2023-02-21 18:02:24 +0200

    Update footer.js

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -4,6 +4,13 @@ import * as styles from '../styles/layout.module.scss';
 
 const Footer = () => {
   return (
+    <div style=
+    {{fontSize: '25px',
+color:black,
+fontWeight: 'bold',
+top:'450px',
+left:'20px',
+position:'absolute'}}>Supported by Edward and Raducu (Bionic Royals)</div>
     <div className={styles.footer}>
       <img src={sponsori} alt='sponsori' />
     </div>
